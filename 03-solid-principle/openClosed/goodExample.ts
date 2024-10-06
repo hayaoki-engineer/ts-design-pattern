093a8c8b0de9c0f55f0a64a8b9428e4d5d1b99d2
Taisei <taisei.960420@gmail.com>
2024-10-06 09:41:17 +0900

    オープンクローズドの原則

--- 03-solid-principle/openClosed/goodExample.ts
+++ 03-solid-principle/openClosed/goodExample.ts
@@ -6,32 +6,32 @@ interface IEmployee {
 }
 
 class JuniorEmployee implements IEmployee {
-    constructor(public name: string) {}
-
+    constructor(public name: string) { }
+    
     getBonus(base: number): number {
         return Math.floor(base * 1.1);
     }
 }
 
 class MiddleEmployee implements IEmployee {
-    constructor(public name: string) {}
-
+    constructor(public name: string) { }
+    
     getBonus(base: number): number {
         return Math.floor(base * 1.5);
     }
 }
 
 class SeniorEmployee implements IEmployee {
-    constructor(public name: string) {}
-
+    constructor(public name: string) { }
+    
     getBonus(base: number): number {
         return Math.floor(base * 2);
     }
 }
 
 class ExpertEmployee implements IEmployee {
-    constructor(public name: string) {}
-
+    constructor(public name: string) { }
+    
     getBonus(base: number): number {
         return Math.floor(base * 3);
     }
@@ -44,10 +44,11 @@ function run() {
     const emp4 = new ExpertEmployee("Bob");
 
     const base = 100;
-    console.log(emp1.getBonus(base));
-    console.log(emp2.getBonus(base));
-    console.log(emp3.getBonus(base));
-    console.log(emp4.getBonus(base));
+
+    console.log(emp1.getBonus(base))
+    console.log(emp2.getBonus(base))
+    console.log(emp3.getBonus(base))
+    console.log(emp4.getBonus(base))
 }
 
 run();
